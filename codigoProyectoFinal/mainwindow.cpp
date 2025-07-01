cbac3b3a08e94b2c372ff7a09de064f0a9adf319
EdwinGutierrezAcevedo <edwin.gutierrez1@udea.edu.co>
2025-07-01 16:26:53 -0500

    se adicionan las imágenes del fondo del primer nive

--- codigoProyectoFinal/mainwindow.cpp
+++ codigoProyectoFinal/mainwindow.cpp
@@ -8,6 +8,14 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
     scene = new QGraphicsScene();
     ui->graphicsView->setScene(scene);
+    QImage imagenFondo(":/img/Base.gif");
+    if (imagenFondo.isNull()) {
+        qDebug() << "La imagen no se cargó correctamente";
+    }
+    QBrush brochaFondo(imagenFondo);
+    ui->graphicsView->setBackgroundBrush(brochaFondo);
+    ui->graphicsView->scale(2,2);
+    scene->setSceneRect(600,245,420,170);
 
 }
 
